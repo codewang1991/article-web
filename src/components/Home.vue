761480d261eacca47c528bd84b917403b7a3f3d3
codewang <1076953055@qq.com>
2019-10-26 17:21:56 +0800

    1.引入vuex， 2.配置路由

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>首页</h2>
+    <router-link to="/main">main</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home'
+import Main from "../components/Main";
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home,
+    children: [
+      {
+        path: '/main',
+        component: Main
+      }
+    ]
+  }
+]
+const router = new Router({
+  mode: 'history',
+  routes:routes
 })
+
+export default router
